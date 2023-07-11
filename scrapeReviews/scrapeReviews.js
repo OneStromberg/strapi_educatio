4acb5ecafd82627c3a703b6898bd38afc47c3088
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-07-12 01:40:30 +0400

    added check for same reviews

--- scrapeReviews/scrapeReviews.js
+++ scrapeReviews/scrapeReviews.js
@@ -1,38 +1,66 @@
 require('dotenv').config();
 // const cron = require('node-cron');
 const puppeteer = require('puppeteer');
-const review = require('../src/api/review/controllers/review');
 const axios = require('axios').default;
 
 async function scrapeAndSaveReviews() {
   try {
     console.log('Функция scrapeReviews выполняется автоматически.');
     const browser = await puppeteer.launch({ headless: true });
+    const apiUrl = process.env.API_HOST;
+    const apiKey = process.env.API_TOKEN;
     const page = await browser.newPage();
 
     await page.goto('https://www.google.com/maps/place/%D0%A6%D0%95+-+%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B8+%D0%95%D0%B4%D1%83%D0%BA%D0%B0%D1%86%D1%96%D1%97/@49.832689,24.0122356,12z/data=!4m8!3m7!1s0x473add32088aee89:0xac4ea9a7960ede46!8m2!3d49.832689!4d24.0122356!9m1!1b1!16s%2Fg%2F11r7w1cp8_?entry=ttu');
 
-    await page.waitForSelector('.XltNde');
+    await page.waitForSelector('.bJzME.tTVLSc');
 
     await page.evaluate(async () => {
-      const reviewElement = document.querySelector('.e07Vkf.kA9KIf');
-      await reviewElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      const reviewElement = document.querySelector('.m6QErb.DxyBCb.kA9KIf.dS8AEf');
 
       await new Promise((resolve) => {
         const scrollInterval = setInterval(() => {
-          window.scrollBy(0, window.innerHeight);
-          if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+          reviewElement.scrollTop += reviewElement.clientHeight;
+          if (reviewElement.scrollHeight - reviewElement.scrollTop === reviewElement.clientHeight) {
             clearInterval(scrollInterval);
             resolve();
           }
-        }, 100);
+        }, 1500);
       });
     });
 
-    const reviews = await page.evaluate(() => {
+    // await page.waitForTimeout(3000);
+
+    const reviews = await page.evaluate(async (apiUrl, apiKey) => {
+
+      async function checkIfReviewExists(ReviewerName) {
+
+        const response = await fetch(
+          `http://${apiUrl}/reviews`,
+          {
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+
+        const reviews = await response.json();
+
+        for (const review of reviews.data) {
+          if (review.attributes.ReviewerName === ReviewerName) {
+            return true;
+          }
+        }
+
+        return false;
+      }
+
       const reviewElements = Array.from(document.querySelectorAll('.jftiEf '));
 
-      return reviewElements.map((reviewElement) => {
+      const filteredReviews = [];
+      for (const reviewElement of reviewElements) {
         const authorElement = reviewElement.querySelector('.d4r55');
         const ReviewerName = authorElement ? authorElement.textContent : '';
 
@@ -41,31 +69,36 @@ async function scrapeAndSaveReviews() {
 
         const commentElement = reviewElement.querySelector('.wiI7pd');
         const ReviewText = commentElement ? commentElement.textContent : '';
-        return {
-          ReviewerName,
-          ReviewText,
-          rating,
-        };
-      });
-    });
+
+        // Проверка и фильтрация отзывов
+        if (!(await checkIfReviewExists(ReviewerName))) {
+          filteredReviews.push({
+            ReviewerName,
+            ReviewText,
+            rating,
+          });
+        }
+      }
+
+      return filteredReviews;
+    }, apiUrl, apiKey);
+
     await browser.close();
 
     // console.log(reviews)
 
-    const apiUrl = process.env.API_HOST;
-    const apiKey = process.env.API_TOKEN;
     for (const reviewData of reviews) {
       await axios.post(
         `http://${apiUrl}/reviews`,
         {
-          data: reviewData
+          data: reviewData,
         },
         {
           headers: {
             Authorization: `Bearer ${apiKey}`,
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          }
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
         }
       );
     }
@@ -81,4 +114,4 @@ async function scrapeAndSaveReviews() {
 //     await scrapeAndSaveReviews();
 // });
 
-scrapeAndSaveReviews(); //для запуска при тестах
+scrapeAndSaveReviews()
