9c3147e12a0468339a093b119fbf48a64bc230fe
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-09-04 17:06:33 +0400

    actualized version of node

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -3,15 +3,12 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::blog-post.blog-post', ({ strapi }) => ({
 
-    // Method 3: Replacing a core action
     async findOne(ctx) {
         const { id } = ctx.params;
         const entity = await strapi.db.query('api::blog-post.blog-post').findOne({
             where: { slug: id },
             populate: ['headingImage']
         });
-        // const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-
         return entity;
     }
 }));
