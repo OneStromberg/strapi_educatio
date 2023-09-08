ebd999f57e9d9bdb7715ddbaf2b7834302ef2b04
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-09-08 23:04:45 +0400

    added preferences and footer table

---
+++ src/api/site-preference/controllers/site-preference.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * site-preference controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::site-preference.site-preference');
---
+++ src/api/site-preference/routes/site-preference.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * site-preference router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::site-preference.site-preference');
