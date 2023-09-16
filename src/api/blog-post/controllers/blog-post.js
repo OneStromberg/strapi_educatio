c0c865ee586dfdb2fcc1de811a8ea975a71e272c
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-09-17 00:32:18 +0400

    internationalization update

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -1,20 +1,28 @@
-'use strict';
-const { createCoreController } = require('@strapi/strapi').factories;
+'use strict'
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::blog-post.blog-post', ({ strapi }) => ({
+module.exports = createCoreController(
+	'api::blog-post.blog-post',
+	({ strapi }) => ({
+		async findOne(ctx) {
+			const { slug } = ctx.params
+			const { locale } = ctx.query
 
-    async findOne(ctx) {
-        const { slug } = ctx.params;
-        const entity = await strapi.db.query('api::blog-post.blog-post').findOne({
-            where: { slug: slug },
-            populate: ['headingImage']
-        });
-        if (entity) {
-            return entity;
-        }
-        else {
-            ctx.status = 404;
-            return 'Not Found';
-        }
-    }
-}));
+			const filters = {
+				where: { slug: slug, locale },
+				populate: ['headingImage'],
+			}
+
+			const entity = await strapi.db
+				.query('api::blog-post.blog-post')
+				.findOne(filters)
+
+			if (entity) {
+				return entity
+			} else {
+				ctx.status = 404
+				return 'Not Found'
+			}
+		},
+	})
+)
